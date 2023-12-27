0562045505e48892fc8ad1ec50dee0306e0cd137
ABOUZIANE <aminebouz84@gmail.com>
2023-12-27 16:14:59 +0100

    add secret and gitignore

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,18 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 const options = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: String(process.env.GOOGLE_CLIENT_ID),
       clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
     }),
   ],
-
+  secret: String(process.env.NEXTAUTH_SECRET),
   // A database is optional, but required to persist accounts in a database
-  //   database: process.env.DATABASE_URL,
-};
+  // database: process.env.DATABASE_URL,
+} as NextAuthOptions;
 
 export default NextAuth(options);
